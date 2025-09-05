51c0cfa2582d9b5120e62f01c6f1e97d019f27cd
CanseFr <jucansell@gmail.com>
2025-09-05 10:37:12 +0200

    :recycle: Get error 500 and 404 in dashboard sentinel

--- src/pages/shop/shop-home/ShopHome.js
+++ src/pages/shop/shop-home/ShopHome.js
@@ -7,6 +7,17 @@ import {useTheme} from "../../../context/Theme";
 export default function ShopHome() {
     const {titreSite, user} = useTheme()
 
+    const generateErrorForDashboard = async (path: string, n: number = 10) => {
+        for (let i = 0; i < n; i++) {
+            try {
+                await fetch(path, {cache: "no-store"});
+            } catch (e) {
+                console.error("Erreur lors de la génération :", e);
+            }
+        }
+    }
+
+
     return (
         <>
             <ShopNavBar/>
@@ -16,6 +27,14 @@ export default function ShopHome() {
                     <Typography component="h2" variant="h5">bienvenue sur votre boutique {titreSite}</Typography>
                 </Grid>
             </Container>
+            <Grid sx={{marginTop: "20px"}}>
+                <button onClick={() => generateErrorForDashboard("/debug-500", 5)}>
+                    Générer 500
+                </button>
+                <button onClick={() => generateErrorForDashboard("/debug-404", 5)}>
+                    Générer 404
+                </button>
+            </Grid>
         </>
     )
 }
